8329eeb896587055859e960a36aa59c0e810c6c0
Joseph Ribbe <social@coderwelsch.com>
2016-12-27 13:13:41 +0100

    Added SteamFree2PlaySuggests node class

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const FbAnalyzer = require( "./scripts/nodejs/fb-analyzer/index.js" );
+const
+	FbAnalyzer = require( "./scripts/nodejs/fb-analyzer/index.js" ),
+	SteamF2P = require( "./scripts/nodejs/steam-f2p-suggests/index.js" );
 
-let fbAnalyzer = new FbAnalyzer();
+let
+	fbAnalyzer = new FbAnalyzer(),
+	steamF2P = new SteamF2P( true );
+
+steamF2P.getRandomGame( ( error, gameData ) => {
+	if ( error ) {
+		console.log( error );
+	} else {
+		console.log( "DONE" );
+		console.log( gameData );
+	}
+} );
--- scripts/nodejs/fb-analyzer/index.js
+++ scripts/nodejs/fb-analyzer/index.js
@@ -4,115 +4,15 @@ const
 	FbGraph = require( "fbgraph" );
 
 
-class FbAnalyzer {
+module.exports = class FbAnalyzer {
 	constructor () {
 		this.expressServer = null;
 		this.authUrl = "";
 
 		this.init();
-		this.configureRoutes();
-		this.authorize();
 	}
 
 	init () {
-		this.expressServer = Express.createServer();
-		this.expressServer.configure(function(){
-			this.expressServer.set('views', __dirname + '/views');
-			this.expressServer.set('view engine', 'jade');
-			this.expressServer.use(Express.bodyParser());
-			this.expressServer.use(Express.methodOverride());
-			this.expressServer.use(this.expressServer.router);
-			this.expressServer.use(Express.static(__dirname + '/public'));
-		});
-
-		this.expressServer.configure('development', function(){
-			this.expressServer.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-		});
-
-		this.expressServer.configure('production', function(){
-			this.expressServer.use(express.errorHandler());
-		});
-
-		this.authUrl = FbGraph.getOauthUrl( {
-			client_id: FbConfig.client_id,
-			redirect_uri: FbConfig.redirect_uri
-		} );
-	}
-
-	configureRoutes () {
-		app.get('/', function(req, res){
-			res.render("index", { title: "click link to connect" });
-		});
-
-		app.get('/auth/facebook', function(req, res) {
-
-		// we don't have a code yet
-		// so we'll redirect to the oauth dialog
-		if (!req.query.code) {
-		var authUrl = graph.getOauthUrl({
-		"client_id":     conf.client_id
-		, "redirect_uri":  conf.redirect_uri
-		, "scope":         conf.scope
-		});
-
-		if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
-		res.redirect(authUrl);
-		} else {  //req.query.error == 'access_denied'
-		res.send('access denied');
-		}
-		return;
-		}
-
-		// code is set
-		// we'll send that and get the access token
-		graph.authorize({
-		"client_id":      conf.client_id
-		, "redirect_uri":   conf.redirect_uri
-		, "client_secret":  conf.client_secret
-		, "code":           req.query.code
-		}, function (err, facebookRes) {
-		res.redirect('/UserHasLoggedIn');
-		});
-
-
-		});
-
-
-		// user gets sent here after being authorized
-		app.get('/UserHasLoggedIn', function(req, res) {
-		res.render("index", { title: "Logged In" });
-		});
-
-
-		var port = process.env.PORT || 3000;
-		app.listen(port, function() {
-		console.log("Express server listening on port %d", port);
-		});
-	}
-
-	authorize () {
 
 	}
-}
-
-module.exports = FbAnalyzer;
-
-/*
-
-// get authorization url
-
-    // shows dialog
-    res.redirect(authUrl);
-
-    // after user click, auth `code` will be set
-    // we'll send that and get the access token
-    graph.authorize({
-        "client_id":      conf.client_id
-      , "redirect_uri":   conf.redirect_uri
-      , "client_secret":  conf.client_secret
-      , "code":           req.query.code
-    }, function (err, facebookRes) {
-      res.redirect('/loggedIn');
-    });
-
-*/
+};
